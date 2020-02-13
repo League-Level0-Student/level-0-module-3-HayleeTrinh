7adc148122381990bd6df6661d783b2e512231b4
CalebVia <55600946+CalebVia@users.noreply.github.com>
2020-02-13 15:57:22 -0800

    finished module 2 Work on CCL and RCC

--- src/_01_robot_color_chooser/RobotColorChooser.java
+++ src/_01_robot_color_chooser/RobotColorChooser.java
@@ -4,16 +4,38 @@
 package _01_robot_color_chooser;
 
 import javax.swing.JOptionPane;
-import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		//1. Create a new Robot
-		
+		Robot bo = new Robot();
 		//3. Ask the user what color they would like the robot to draw
+		String col = JOptionPane.showInputDialog("What color out of ROYGBP is you favorite?");
 		
 		//5. Use an if/else statement to set the pen color that the user requested
-
+		if(col.equalsIgnoreCase("Red")) {
+			bo.setPenColor(255, 4, 0);
+		}
+		else if(col.equalsIgnoreCase("Orange")) {
+			bo.setPenColor(255, 132, 0);
+		}
+		else if(col.equalsIgnoreCase("Yellow")) {
+			bo.setPenColor(255, 247, 0);
+		}
+		else if(col.equalsIgnoreCase("Green")) {
+			bo.setPenColor(97, 242, 143);
+		}
+		else if(col.equalsIgnoreCase("Blue")) {
+			bo.setPenColor(133, 226, 255);
+		}
+		else if(col.equalsIgnoreCase("Purple")) {
+			bo.setPenColor(181, 107, 255);
+		}
+		else {
+			bo.setRandomPenColor();
+		}
         //6. If the user doesn’t enter anything, choose a random color
 
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
